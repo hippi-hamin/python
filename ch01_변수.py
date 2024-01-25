22555d77d6b87eb5d60fc15e22b692a855b7520f
hippi-hamin <9908haha@naver.com>
2024-01-25 23:26:58 +0900

    first commit

---
+++ ch01_변수.py
@@ -0,0 +1,24 @@
+# 변수에 값 저장
+age = 21
+name = "홍길동"
+print(age)
+print(name)
+
+# 값 변경
+age = 50
+name = "김길동"
+print(age)
+print(name)
+
+# # 값 복사
+age1 = age
+print(age1)
+
+# 새로운 값 저장 방법
+a = 5
+b = 6
+print(a)
+print(b)
+a, b = 7, 8
+print(a)
+print(b)
